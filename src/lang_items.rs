38968fa25e13024e342170e6278f4be5f890bd9b
yzs <320016@qq.com>
2025-09-22 19:36:09 +0800

    feat: update panic use shutdown

--- src/lang_items.rs
+++ src/lang_items.rs
@@ -1,7 +1,19 @@
 
 use core::panic::PanicInfo;
 
+use crate::{println, sbi};
+
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    loop{}
+    if let Some(location) = _info.location() {
+        println!("Panic at {}, line: {}, column: {}, due to {}.", 
+            location.file(),
+            location.line(),
+            location.column(),
+            _info.message()
+        );
+    } else {
+        println!("Panic due to {}", _info.message());
+    }
+    sbi::shutdown(true);
 }
